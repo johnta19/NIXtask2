c2c93de9bb3c9eafd78f82feee8d50714e0d8dd6
Johnta19 <m.eigster20@gmail.com>
2021-06-25 18:08:26 +0300

    hometask2

--- src/com/Alevel/Main.java
+++ src/com/Alevel/Main.java
@@ -1,9 +1,28 @@
 package com.Alevel;
 
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
+        int[] arraySorted;
+        StringArray stringArray = new StringArray(5, 1, 2, 3, 5, 9, 7, 0);
+        stringArray.printArray(stringArray.myArr);
 
+        InsertionSort sorts = new InsertionSort();
+        arraySorted = sorts.insertionSortASC(stringArray.myArr);
+        System.out.println(Arrays.toString(arraySorted));
+
+        arraySorted = sorts.insertionSortDESC(stringArray.myArr);
+        System.out.println(Arrays.toString(arraySorted));
+
+        numberedArray(stringArray.myArr);
+    }
+
+    static int[] numberedArray(int[] stringArray){
+        for (int i = 0; i < stringArray.length; i++) {
+            System.out.print(stringArray[i] + " : " + (i + 1) + " element; ");
+        }
+        return stringArray;
     }
 }
